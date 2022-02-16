f1c6a300741c9bf5cbfc890730fe2f72a22922a3
chenxiahuaxu <863025212@qq.com>
2022-02-16 17:59:18 +0800

    调用transform.py的路径错误

--- tools/transform.bat
+++ tools/transform.bat
@@ -7,7 +7,7 @@ set TRANS_DIR=%PROJECT_ROOT%\platform\Air72x\core
 call tools\launch.bat
 
 :: 转换头文件
-python38 %TRANS% transform -i "%TRANS_DIR%\core_api.h" -o "%TRANS_DIR%\trans_api\core_api.h" -oc "%TRANS_DIR%\trans_api\core_api.c"
+python38 %TRANS% transform -i "%TRANS_DIR%\core_api.h" -o "%TRANS_DIR%\trans_out\core_api.h" -oc "%TRANS_DIR%\trans_out\core_api.c"
 
 :: 移动无函数定义的头文件，主要定义一些类型
 :: python38 %TRANS% "move" -f "%CORE_INC%\cs_types.h" "%CORE_INC%\am_openat_drv.h" -d %USER_INC%
